9f363f50dc6fb9865158b21d6cd0f10275d83c89
kutikq <kutenkov5yandex.ru@mail.ru>
2024-12-16 01:34:52 +0300

    +top100 и search

--- import telebot.py
+++ import telebot.py
@@ -12,16 +12,35 @@ bot = telebot.TeleBot(TOKEN)
 df = pd.read_csv('anime.csv')
 # Удалить строки с пропущенными значениями
 df_cleaned = df.dropna()
-print(df_cleaned.info())
 
 # Просмотрите первые 5 строк
 print(df.head())
 
 # Декоратор команды /start
-@bot.message_handler(commands=['start'])
+@bot.message_handler(commands = ['start'])
 def send_welcome(message):
     bot.reply_to(message, "Я тупой ботяра, не судите строго, напишем пару заметок(хз).")
 
+# Поиск по названию
+def search_by_name(name):
+    result = df[df['name'].str.contains(name, case = False, na = False)]
+    return result
+# Поиск по жанру
+def search_by_genre(genre):
+    result = df[df['genre'].str.contains(genre, case = False, na = False)]
+    return result
+
+# Вывод топ 100 по популярности
+def show_top100_rait():
+    top_100 = df.sort_values(by = 'rating')
+    result = top_100.head(100)
+    return result
+
+# Топ 100 по зрителям
+def show_top100_rait():
+    top_100 = df.sort_values(by = 'members')
+    result = top_100.head(100)
+    return result
 
 
 # Запуск бота
